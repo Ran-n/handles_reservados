79223876c85ea113fd0869b1c7f4935bdf83d7b3
ran <ran-n@tutanota.com>
2022-01-28 21:12:12 +0100

    arranxado chaves para ambos so

--- chaves.py
+++ chaves.py
@@ -3,17 +3,27 @@
 # ------------------------------------------------------------------------------
 #+ Autor:  	Ran#
 #+ Creado: 	2022/01/27 18:42:07.263328
-#+ Editado:	2022/01/28 19:02:29.410187
+#+ Editado:	2022/01/28 21:11:44.494888
 # ------------------------------------------------------------------------------
 import secrets
 import os
+import sys
 # ------------------------------------------------------------------------------
-while True:
+sistema_operativo = sys.platform
+
+seguir = True
+while seguir:
+    if sistema_operativo == 'win32':
+        os.system('cls')
+    elif sistema_operativo == 'linux':
+        os.system('clear')
+
     print()
+
     for i in range(1, 20):
         print(secrets.token_urlsafe(32)[:32])
 
     print()
-    input('Intro para máis chaves')
-    os.system('cls')
+    if input('Intro para máis chaves') == '.': seguir = False
+
 # ------------------------------------------------------------------------------
